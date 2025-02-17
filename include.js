15fbf12b289a89cb3c2089526f4b0b70e51f9430
nobu0903 <kareta09badominton@gmail.com>
2025-02-17 17:12:51 +0100

    include.js追加

---
+++ include.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //ヘッダーを読み込む
+    fetch("./partials/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data;
+        })
+        .catch(error => console.error("ヘッダーの読み込みに失敗しました:", error));
+
+     // フッターを読み込む
+     fetch("./partials/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        })
+        .catch(error => console.error("フッターの読み込みに失敗:", error));
+});
